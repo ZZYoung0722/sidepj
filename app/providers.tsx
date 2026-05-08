d7d5e7e523cf4c316b89aa99b975a3ccbe56f6a6
ZZYoung0722 <dlwldud0722@gmail.com>
2026-05-08 20:00:19 +0900

    - TanStack Query 사용할 수 있는 준비 과정 파일 구성 - Supabase 연결 설정 하는 파일 구성

---
+++ app/providers.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { useState } from 'react'
+
+/*
+TanStack Query 쓸 수 있게 앱 전체 감싸주는 설정
+준비과정
+ */
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient())
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
+}
